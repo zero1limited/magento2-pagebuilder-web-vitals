56b77fd156d5929bc2d655ab187c3956868b3173
sean-zero1 <sean.watson@zero1.co.uk>
2022-07-27 13:59:42 +0100

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Zero1_PageBuilderWebVitalsLazy',
+        __DIR__
+    );
